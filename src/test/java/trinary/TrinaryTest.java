b23f0dd52a66742fde3eed2717ea3ed8b4d481ad
Jeremy M. Reed <reeje76@gmail.com>
2016-06-30 15:07:02 -0600

    Add extra unit tests.

--- src/test/java/trinary/TrinaryTest.java
+++ src/test/java/trinary/TrinaryTest.java
@@ -1,10 +1,28 @@
 package trinary;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TrinaryTest {
-    @Test
+    
+	@Test
+	public void test_toDecimal_NullString_ThrowsException() {
+		try {
+			Trinary.toDecimal( null );
+			fail("Supposed to catch NullPointerException");
+		} catch ( NullPointerException npe ) {
+			
+		} catch ( Exception e ) {
+			fail("Expected to catch NullPointerException");
+		}
+	}
+	
+	@Test
+    public void testNonTrinaryEmptyStringIsZero() {
+        assertEquals(0, Trinary.toDecimal(""));
+    }
+	
+	@Test
     public void testNonTrinaryCharacterIsZero() {
         assertEquals(0, Trinary.toDecimal("-"));
     }
