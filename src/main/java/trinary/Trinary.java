9aa007240aa443543ddec8adcce588808e0cc4a1
Jeremy M. Reed <reeje76@gmail.com>
2016-06-30 14:56:14 -0600

    Project setup.
    Import into Eclipse.
    Convert to Maven project.
    Add JUnit 4.10 dependency.
    Move sources to proper packages.
    Add NotImplementedException RuntimeException.
    Stub out Trinary methods.

---
+++ src/main/java/trinary/Trinary.java
@@ -0,0 +1,10 @@
+package trinary;
+
+import exceptions.NotImplementedException;
+
+public class Trinary
+{
+	public static int toDecimal(String input) {
+		throw new NotImplementedException();
+	}
+}
